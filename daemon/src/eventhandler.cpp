745f338e96ddf9adc773011992222f5dfb712750
Adam Pigg <adam@piggz.co.uk>
2018-11-06 21:51:55 +0000

    Add timer to check for BT device and enumerate input devices

--- daemon/src/eventhandler.cpp
+++ daemon/src/eventhandler.cpp
@@ -8,20 +8,29 @@
 #define SERVICE_NAME "com.piggz.taskswitcher"
 
 EventHandler::EventHandler() {
-    qDebug() << "Eventhandler";
     m_worker = new Worker;
     m_worker->moveToThread(&m_workerThread);
     connect(&m_workerThread, &QThread::finished, m_worker, &QObject::deleteLater);
     connect(this, &EventHandler::start, m_worker, &Worker::readKeyboard);
     connect(m_worker, &Worker::altTabPressed, this, &EventHandler::altTabPressed);
     connect(m_worker, &Worker::altReleased, this, &EventHandler::altReleased);
-
+    connect(m_worker, &Worker::finished, this, &EventHandler::workerFinished);
     m_workerThread.start();
+
+    //Start a timer to check for BT keyboard
+    m_timer = new QTimer(this);
+    connect(m_timer, &QTimer::timeout, this, &EventHandler::checkForDevice);
+    m_timer->start(10000);
 }
 
 EventHandler::~EventHandler() {
     m_workerThread.quit();
     m_workerThread.wait();
+}
+
+void EventHandler::startWorker(const QString &device)
+{
+    start(device);
 }
 
 void EventHandler::altTabPressed()
@@ -55,4 +64,52 @@ void EventHandler::altReleased()
         iface.call("hideTaskSwitcher");
     }
     
+}
+
+void EventHandler::workerFinished()
+{
+    qDebug() << "Worker has finished";
+}
+
+void EventHandler::checkForDevice()
+{
+
+}
+
+QString EventHandler::getDeviceFile(const QString &name)
+{
+    bool name_found = false;
+    QString eventstring;
+    QString devicepath;
+
+    QFile file("/proc/bus/input/devices");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+
+    QTextStream in(&file);
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+
+        if (!name_found)
+        {
+            if (line.contains(name)) {
+                name_found = true;
+            }
+        }
+        else
+        {
+            if (line.contains("event"))
+            {
+                eventstring = line.mid(line.indexOf("event"));
+                eventstring = eventstring.mid(0, eventstring.indexOf(' '));
+                break;
+            }
+        }
+    }
+
+    devicepath = "/dev/input/" + eventstring;
+    qDebug() <<  "devicepath:" << devicepath;
+
+    return devicepath;
+
 }
--- daemon/src/eventhandler.h
+++ daemon/src/eventhandler.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QThread>
+#include <QTimer>
 
 #include "worker.h"
 
@@ -10,19 +11,26 @@ class EventHandler : public QObject
 {
     Q_OBJECT
     
-    public:
+public:
     EventHandler();
     ~EventHandler();
 
-    Q_SLOT void altTabPressed();
-    Q_SLOT void altReleased();
     Q_SIGNAL void start(const QString &device);
     
-    private:
-    
+private:
+    void startWorker(const QString &device);
+
+    Q_SLOT void altTabPressed();
+    Q_SLOT void altReleased();
+    Q_SLOT void workerFinished(); //probably the device disappeared
+    Q_SLOT void checkForDevice();
+
     QThread m_workerThread;
     Worker *m_worker;
+    QTimer *m_timer;
+
     bool m_taskSwitcherVisible  = false;
+    QString getDeviceFile(const QString &name);
 
 };
 
